4c33a16ca6721c333e5daada92c3459e5658e4d7
Billy Gillooley <gillooley@gmail.com>
2024-04-19 13:44:20 +0100

    Added back Lusitana font

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,9 +1,14 @@
 import { Inter } from 'next/font/google';
 import { Playfair_Display } from 'next/font/google';
 import { Lato } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
 export const playfairdisplay = Playfair_Display({
+  weight: '400',
+  subsets: ['latin'],
+});
+export const lusitana = Lusitana({
   weight: '400',
   subsets: ['latin'],
 });
