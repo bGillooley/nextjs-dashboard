7f4e3a4f88c6faee9ebde0d5d34b1de3cbe91947
Billy Gillooley <gillooley@gmail.com>
2024-04-19 13:35:16 +0100

    First stab

---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+import { Inter } from 'next/font/google';
+import { Playfair_Display } from 'next/font/google';
+import { Lato } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const playfairdisplay = Playfair_Display({
+  weight: '400',
+  subsets: ['latin'],
+});
+export const lato = Lato({ weight: '400', subsets: ['latin'] });
